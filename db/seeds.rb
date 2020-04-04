d5e4bbdbf0e2e630782a5fa97609efbba63bfcf1
Marine Sourin <marinesourin@live.fr>
2020-04-05 01:54:19 +0200

    Third commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.destroy_all
-ingredients = [Abricots, Abricots au sirop, Abricots sec]
+ingredients = ["Abricot", "Abricot sec", "Ail", "Amande", "Artichaut", "Asperge", "Aubergine", "Avocat", "Banane", "Basilic", "Bette",
+                "Betterave", "Blanc de dinde", "Blanc de poulet", "Blette", "Brocoli", "Cacahuète", "Carotte", "Céleri", "Champignon",
+                "Choux", "Citrouille", "Comté ", "Concombre", "Cornichon", "Couli de tomate", "Courge", "Courgette", "Crème fraiche",
+                "Crème liquide", "Cresson", "Datte", "Échalote", "Emmental", "Endive", "Épinard", "Fenouil ", "Fève", "Flageolet",
+                "Frisée", "Fromage râpé ", "Gingembre", "Haricot rouge", "Haricot vert", "Jambon", "Ketchup", "Lait", "Lait végétal",
+                "Laitue", "Lardons", "Lentille", "Levure chimique", "Mâche", "Maïs", "Margarine", "Mayonnaise", "Moutarde", "Mozzarella ",
+                "Navet", "Noix", "Noix de cajou", "Noix de coco", "Œufs", "Oignon", "Olive noire", "Olive verte", "Origan", "Pâte à pizza",
+                "Pâte brisée", "Pâte feuilletée", "Petits pois", "Poire", "Poireau", "Poivron jaune", "Poivron rouge", "Poivron vert", "Pomme",
+                "Pomme de terre", "Potimarron", "Potiron", "Radis", "Rhubarbe", "Sauce tomate", "Saumon", "Soja", "Sucre vanillé", "Thon",
+                "Tomate", "Vanille", "Viande rouge", "Yaourt nature"]
 
 ingredients.each do |ingredient|
   Ingredient.create nom: ingredient
